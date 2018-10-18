ac799b39d6b2f6aa6d53222a5910d6c083ab4773
Mike Spano Mac laptop <mikespanomaclaptop@Mikes-MacBook-Pro.local>
2018-10-18 08:46:30 -0400

    Doc String Added

--- menu.py
+++ menu.py
@@ -3,19 +3,29 @@ import time
 import random
 class Menu:
     def __init__(self):
+        '''
+           inits the menu
+        '''
         init = pygame.init()
         self.window = pygame.display.set_mode((800,600))
         self.title = pygame.image.load("Sprites//titlecard.png").convert()
+        # Method Calls
         self.randomize()
         self.start_button()
         self.title_pic()
 
     def randomize(self):
+        '''
+           creates random colors in background
+        '''
         x = random.randint(0, 255)
         x1 = random.randint(0, 255)
         x2 = random.randint(0, 255)
         self.window.fill((x,x1,x2))
     def title_pic(self):
+        '''
+           blits title image onto surface
+        '''
         self.window.blit(self.title, (200,100))
 
 
