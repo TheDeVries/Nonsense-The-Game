34ea63f879eec4d9259a1f9c84933f7b331fc21b
Mike Spano Mac laptop <mikespanomaclaptop@Mikes-MacBook-Pro.local>
2018-10-19 10:30:56 -0400

    Condensed code

--- main_menu.py
+++ main_menu.py
@@ -99,4 +99,30 @@ class Menu:
         pass
     def setting(self):
         pass
-from instructions import *
+class Instructions:
+    def __init__(self):
+        self.window = pygame.display.set_mode((800,600))
+        self.running = True
+
+        # Main Menu Loop
+        while self.running:
+            for event in pygame.event.get():
+                # Quit button
+                if event.type == pygame.QUIT:
+                    foo = Menu()
+                    self.running = False
+
+                # Keybinds
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        bar = Menu()
+                        self.running = False
+
+                self.instruct()
+
+                pygame.display.flip()
+    def instruct(self):
+        self.window.fill((255,255,255))
+        myfont = pygame.font.SysFont('Times New Roman', 30)
+        textsurface = myfont.render('Instructions: Example instructions...', True, (0, 0, 0))
+        self.window.blit(textsurface,(0,0))
