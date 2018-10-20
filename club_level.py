41bc91635896d89b45efa7a4343fe089e16d692c
Brian DeVries <bdevrie1@binghamton.edu>
2018-10-20 16:20:26 -0400

    Added a woman.

--- club_level.py
+++ club_level.py
@@ -1,13 +1,13 @@
 import pygame
 pygame.init()
 
-#Must still make into a class, in the meantime it functions on its own.
 class Club:
     def __init__(self):
         self.running = True
         self.window = pygame.display.set_mode((800, 600))
         self.club_background = pygame.image.load("Sprites//club.png").convert()
         self.bar2 = pygame.image.load("Sprites//empty_bar.png").convert()
+        self.bar_woman = pygame.image.load("Sprites//bar_woman.png").convert()
         self.bar_man = pygame.image.load("Sprites//bar_man.png").convert()
         while self.running == True:
             for event in pygame.event.get():
@@ -20,9 +20,15 @@ class Club:
                     if event.key == pygame.K_ESCAPE:
                         self.running = False
             self.window.blit(self.club_background, (0,0))
+
+            self.bar_woman.set_colorkey((255,255,255))
+            self.bar_woman = pygame.transform.scale(self.bar_woman, (250,500))
+            self.window.blit(self.bar_woman, (375, 90))
+
             self.bar_man.set_colorkey((255,255,255))
             self.bar_man = pygame.transform.scale(self.bar_man, (250, 500))
             self.window.blit(self.bar_man, (250, 150))
+
             self.bar2.set_colorkey((0,0,64))
             self.window.blit(self.bar2, (200, 300))
 
