c25b6b35d79517f737dec7e541b1a76df275e857
Mike Spano Mac laptop <mikespanomaclaptop@Mikes-MacBook-Pro.local>
2018-10-23 17:19:55 -0400

    Changes

--- maze_level.py
+++ maze_level.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Maze:
-    def __inti__(self):
+    def __init__(self):
         self.running = True
         self.wn = pygame.display.set_mode((800,600))
         while self.running:
