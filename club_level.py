0a3b669e6bd4394051b414d05de65fa91d70aae9
Brian DeVries <bdevrie1@binghamton.edu>
2018-10-29 23:32:08 -0400

    further testing (more to come)

--- club_level.py
+++ club_level.py
@@ -6,8 +6,10 @@ pygame.init()
 class Club:
     def __init__(self):
         self.running = True
+        #Club & Characters
         self.window = pygame.display.set_mode((800, 600))
         self.club_background = pygame.image.load("Sprites//club.png").convert()
+        self.club_background2 = pygame.image.load("Sprites//club2.png").convert()
         self.bar = pygame.image.load("Sprites//empty_bar.png").convert()
         self.bar_woman = pygame.image.load("Sprites//bar_woman.png").convert()
         self.bar_woman_front = pygame.image.load("Sprites//bar_woman_front.png").convert()
@@ -20,12 +22,23 @@ class Club:
         self.bar_server = pygame.image.load("Sprites//bar_server.png").convert()
         self.bar_server_front = pygame.image.load("Sprites//bar_server_front.png").convert()
 
+        #Keyboard DDR Setting
         self.speech_bubble = pygame.image.load("Sprites//speech_bubble.png").convert()
-        self.arrow_up = pygame.image.load("Sprites//Up_Arrow.png").convert()
-        self.arrow_down = pygame.image.load("Sprites//Down_Arrow.png").convert()
-        self.arrow_right = pygame.image.load("Sprites//Right_Arrow.png").convert()
-        self.arrow_left = pygame.image.load("Sprites//Left_Arrow.png").convert()
-        self.window.blit(self.club_background, (0,0))
+        self.arrow = pygame.image.load("Sprites//arrow.png").convert()
+        self.arrow_orange = pygame.image.load("Sprites//arrow_orange.png").convert()
+
+        self.arrow.set_colorkey((255,255,255))
+        arrow_left = self.arrow
+        arrow_up = pygame.transform.rotate(self.arrow, 270)
+        arrow_down = pygame.transform.rotate(self.arrow, 90)
+        arrow_right = pygame.transform.rotate(self.arrow, 180)
+        la_left = self.arrow_orange
+        la_up = pygame.transform.rotate(self.arrow_orange, 270)
+        la_down = pygame.transform.rotate(self.arrow_orange, 90)
+        la_right = pygame.transform.rotate(self.arrow_orange, 180)
+
+        self.window.blit(self.club_background2, (0,0))
+        #Must find way to randomize positions and put reliabe buttons at those positions.
         '''
         self.chosens = self.Randomize()
         '''
@@ -65,10 +78,14 @@ class Club:
                 self.window.blit(self.bar_server2, (550, 200))
             '''
 
-            self.bar_man_front.set_colorkey((255,255,255))
-            self.window.blit(self.bar_man_front, (400, 300))
+            self.bar_woman2_front.set_colorkey((255,255,255))
+            self.window.blit(self.bar_woman2_front, (400, 300))
             self.speech_bubble.set_colorkey((255,255,255))
             self.window.blit(self.speech_bubble, (400, 0))
+            self.window.blit(la_up, (100, 43))
+            self.window.blit(la_down, (200, 43))
+            self.window.blit(la_left, (0, 43))
+            self.window.blit(la_right, (300, 43))
 
             Controller.sanity_meter(self, self.window)
             pygame.display.flip()
