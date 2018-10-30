865842cd9dc2bb49ef94829eb811b6cd7a8868ab
Brian DeVries <bdevrie1@binghamton.edu>
2018-10-29 22:46:11 -0400

    Testing placements

--- club_level.py
+++ club_level.py
@@ -10,14 +10,25 @@ class Club:
         self.club_background = pygame.image.load("Sprites//club.png").convert()
         self.bar = pygame.image.load("Sprites//empty_bar.png").convert()
         self.bar_woman = pygame.image.load("Sprites//bar_woman.png").convert()
+        self.bar_woman_front = pygame.image.load("Sprites//bar_woman_front.png").convert()
         self.bar_woman2 = pygame.image.load("Sprites//bar_woman2.png").convert()
+        self.bar_woman2_front = pygame.image.load("Sprites//bar_woman2_front.png").convert()
         self.bar_man = pygame.image.load("Sprites//bar_man.png").convert()
+        self.bar_man_front = pygame.image.load("Sprites//bar_man_front.png").convert()
         self.bar_man2 = pygame.image.load("Sprites//bar_man2.png").convert()
+        self.bar_man2_front = pygame.image.load("Sprites//bar_man2_front.png").convert()
         self.bar_server = pygame.image.load("Sprites//bar_server.png").convert()
+        self.bar_server_front = pygame.image.load("Sprites//bar_server_front.png").convert()
+
+        self.speech_bubble = pygame.image.load("Sprites//speech_bubble.png").convert()
+        self.arrow_up = pygame.image.load("Sprites//Up_Arrow.png").convert()
+        self.arrow_down = pygame.image.load("Sprites//Down_Arrow.png").convert()
+        self.arrow_right = pygame.image.load("Sprites//Right_Arrow.png").convert()
+        self.arrow_left = pygame.image.load("Sprites//Left_Arrow.png").convert()
         self.window.blit(self.club_background, (0,0))
-
+        '''
         self.chosens = self.Randomize()
-
+        '''
         while self.running == True:
             for event in pygame.event.get():
                 # Quit button
@@ -29,7 +40,7 @@ class Club:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         exit()
-
+            '''
             if self.chosens[5] == self.bar_server:
                 pass
             else:
@@ -52,6 +63,13 @@ class Club:
                 self.chosens[5].set_colorkey((255,255,255))
                 self.bar_server2 = pygame.transform.scale(self.chosens[5], (250, 500))
                 self.window.blit(self.bar_server2, (550, 200))
+            '''
+
+            self.bar_man_front.set_colorkey((255,255,255))
+            self.window.blit(self.bar_man_front, (400, 300))
+            self.speech_bubble.set_colorkey((255,255,255))
+            self.window.blit(self.speech_bubble, (400, 0))
+
             Controller.sanity_meter(self, self.window)
             pygame.display.flip()
 
