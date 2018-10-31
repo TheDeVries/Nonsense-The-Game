e9fced4c8ad0c926d08967575814126541c72003
Mike Spano Mac laptop <mikespanomaclaptop@Mikes-MacBook-Pro.local>
2018-10-31 12:17:05 -0400

    major changes

--- controller.py
+++ controller.py
@@ -54,6 +54,27 @@ class Controller:
         score_surface = myfont.render(scores, True, color)
         window.blit(textsurface,(500,0))
         window.blit(score_surface,(600,0))
+class SpriteSheet(object):
+
+    def __init__(self, file_name):
+
+        # Load the sprite sheet.
+        self.sprite_sheet = pygame.image.load(file_name).convert()
+
+
+    def get_image(self, x, y, width, height, color_key):
+
+        # Create a new blank image
+        image = pygame.Surface([width, height]).convert()
+
+        # Copy the sprite from the large sheet onto the smaller image
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
+
+        # Assuming pink works as the transparent color
+        image.set_colorkey((color_key))
+
+        # Return the image
+        return image
 
 
 from club_level import *
